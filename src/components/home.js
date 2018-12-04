e4c5ddd9890400b9338ba33f8e0c17f155413574
Collin <cstewa91@gmail.com>
2018-12-04 13:36:32 -0800

    Finished project-setup

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => {
+   return (
+      <div className="center">
+         <h1>Chatter App</h1>
+         <p className="grey-text">Just some text</p>
+      </div>
+   )
+}
